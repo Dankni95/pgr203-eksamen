9f17b2e41634b88431e1bcf141d0b073c78a5645
jannickeham <jannicke.hamarsnes@gmail.com>
2021-11-12 20:16:34 +0100

    added tests (green) to optionDaoTest

--- src/test/java/no/kristiania/dao/OptionDaoTest.java
+++ src/test/java/no/kristiania/dao/OptionDaoTest.java
@@ -1,31 +1,99 @@
 package no.kristiania.dao;
 
 import no.kristiania.entity.Option;
+import no.kristiania.entity.Question;
+import no.kristiania.entity.Survey;
 import org.junit.jupiter.api.Test;
+
 import java.sql.SQLException;
-import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class OptionDaoTest {
+    private final OptionDao optionDao = new OptionDao(TestData.testDataSource());
+    private static Option option;
+    private static Survey survey;
+    private static Question question;
 
 
-class OptionDaoTest {
-    private final OptionDao dao = new OptionDao(TestData.testDataSource());
+
+    public static Option exampleQuestion() throws SQLException {
+        QuestionDao questionDao = new QuestionDao(TestData.testDataSource());
+        OptionDao optionDao = new OptionDao(TestData.testDataSource());
+        SurveyDao surveyDao = new SurveyDao(TestData.testDataSource());
+
+        survey = new Survey();
+        survey.setUserId(1); // Annon
+        survey.setTitle("Test survey name");
+        surveyDao.save(survey);
+
+        question = new Question();
+        question.setTitle("Test question name");
+        question.setText("Test subtitle name");
+        question.setUserId(1);
+        question.setSurveyId(survey.getId());
+
+        questionDao.save(question);
+
+        option = new Option();
+        option.setTitle("Option 1");
+        option.setQuestionId(question.getId());
+
+        Option option2 = new Option();
+        option2.setTitle("Option 2");
+        option2.setQuestionId(question.getId());
+
+        Option option3 = new Option();
+        option3.setTitle("Option 3");
+        option3.setQuestionId(question.getId());
+
+        Option option4 = new Option();
+        option4.setTitle("Option 4");
+        option4.setQuestionId(question.getId());
+
+        Option option5 = new Option();
+        option5.setTitle("Option 5");
+        option5.setQuestionId(question.getId());
+
+
+        optionDao.save(option);
+        optionDao.save(option2);
+        optionDao.save(option3);
+        optionDao.save(option4);
+        optionDao.save(option5);
+
+        return option;
+    }
 
     @Test
-    void shouldListSavedOptions() throws SQLException {
-        String option1 = "option-" + UUID.randomUUID();
-        String option2 = "option-" + UUID.randomUUID();
+    void shouldRetrieveSavedOption() throws SQLException {
+        Option option = exampleQuestion();
+        optionDao.save(option);
+        assertThat(optionDao.retrieve(option.getId()))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(option);
+    }
 
-        Option option = new Option();
-        option.setTitle(option1);
-        option.setQuestionId(1);
-        dao.save(option);
+    @Test
+    void shouldListAllOptions() throws SQLException {
+        Option option = exampleQuestion();
+        optionDao.save(option);
+        Option anotherOption = exampleQuestion();
+        optionDao.save(anotherOption);
 
-        Option op = new Option();
-        op.setTitle(option2);
-        dao.save(op);
+        assertThat(optionDao.listAll())
+                .extracting(Option::getId)
+                .contains(option.getId(), anotherOption.getId());
+    }
 
-        assertThat(dao.listAll())
-                .extracting(Option::getTitle)
-                .contains(option.getTitle(), op.getTitle());
+    @Test
+    void shouldDeleteOptions() throws SQLException {
+        Option option = exampleQuestion();
+        optionDao.save(option);
+        optionDao.deleteAll();
+
+        assertNull(optionDao.retrieve(option.getId()));
     }
 }
